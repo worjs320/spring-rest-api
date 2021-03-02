a20af57a921816aa0f18a4f803a192649a7f80a7
worjs320 <worjs320@naver.com>
2021-03-03 00:41:43 +0900

    Initial commit

---
+++ src/main/java/com/rest/api/HelloController.java
@@ -0,0 +1,36 @@
+package com.rest.api;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping(value = "/helloworld/string")
+    @ResponseBody
+    public String HelloWorldString() {
+        return "helloworld";
+    }
+
+    @GetMapping(value = "/helloworld/json")
+    @ResponseBody
+    public Hello HelloWorldJson() {
+        Hello hello = new Hello();
+        hello.message = "helloworld";
+        return hello;
+    }
+
+    @GetMapping(value = "/helloworld/page")
+    public String HelloWorldPage() {
+        return "helloworld";
+    }
+
+    @Getter
+    @Setter
+    public static class Hello {
+        private String message;
+    }
+}
