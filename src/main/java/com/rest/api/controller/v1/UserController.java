1cd46516dc86601bb7c39fba4e2b5f570b3c9a42
worjs320 <worjs320@naver.com>
2021-03-05 00:34:12 +0900

    feat: Add Swagger api document

--- src/main/java/com/rest/api/controller/v1/UserController.java
+++ src/main/java/com/rest/api/controller/v1/UserController.java
@@ -2,28 +2,34 @@ package com.rest.api.controller.v1;
 
 import com.rest.api.entity.User;
 import com.rest.api.repo.UserJpaRepo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
+@Api(tags = {"1. User"})
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/v1")
 public class UserController {
     private final UserJpaRepo userJpaRepo;
 
+    @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다")
     @GetMapping(value = "/user")
     public List<User> findAllUser() {
         return userJpaRepo.findAll();
     }
 
+    @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
     @PostMapping(value = "/user")
-    public User save(@RequestBody Map<String,String> param) {
+    public User save(@ApiParam(value = "회원 아이디", required = true) @RequestParam String uid,
+                     @ApiParam(value = "회원 이름", required = true) @RequestParam String name) {
         User user = User.builder().
-                uid(param.get("uid")).
-                name(param.get("name")).
+                uid(uid).
+                name(name).
                 build();
         return userJpaRepo.save(user);
     }
