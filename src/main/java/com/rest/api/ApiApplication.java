72a394a35545578effe0eb7b45a5e3a2423e416b
worjs320 <worjs320@naver.com>
2021-03-16 00:29:44 +0900

    feat: Enable JpaAuditing

--- src/main/java/com/rest/api/ApiApplication.java
+++ src/main/java/com/rest/api/ApiApplication.java
@@ -3,11 +3,13 @@ package com.rest.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ApiApplication {
 
     public static void main(String[] args) {
