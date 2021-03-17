931954945640649776af6d4b699c71f29ab23a88
worjs320 <worjs320@naver.com>
2021-03-18 00:32:23 +0900

    feat: Work board rest api

--- src/main/java/com/rest/api/repo/board/PostJpaRepo.java
+++ src/main/java/com/rest/api/repo/board/PostJpaRepo.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PostJpaRepo extends JpaRepository<Post, Long> {
-    List<Post> findByBoardOrderByPostIdDesc(Board board);
+    List<Post> findByBoard(Board board);
 }
