4b55d34e1ec76904cf890d542b8437aa1366b9b4
worjs320 <worjs320@naver.com>
2021-03-19 00:30:57 +0900

    feat: Lazy loading config

--- src/main/java/com/rest/api/entity/User.java
+++ src/main/java/com/rest/api/entity/User.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.rest.api.entity.common.CommonDateEntity;
 import lombok.*;
+import org.hibernate.annotations.Proxy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,6 +23,7 @@ import java.util.stream.Collectors;
 @Table(name = "user") // 'user' 테이블과 매핑됨을 명시
 // Post Entity에서 User와의 관계를 Json으로 변환시 오류 방지를 위한 코드
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Proxy(lazy = false)
 public class User extends CommonDateEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
