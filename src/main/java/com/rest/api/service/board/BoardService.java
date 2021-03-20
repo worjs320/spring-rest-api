ab779e39aebc4db8791189d334313df61931c54e
worjs320 <worjs320@naver.com>
2021-03-20 20:24:43 +0900

    feat: Cache process work

--- src/main/java/com/rest/api/service/board/BoardService.java
+++ src/main/java/com/rest/api/service/board/BoardService.java
@@ -3,6 +3,7 @@ package com.rest.api.service.board;
 import com.rest.api.advice.exception.CNotOwnerException;
 import com.rest.api.advice.exception.CResourceNotExistException;
 import com.rest.api.advice.exception.CUserNotFoundException;
+import com.rest.api.common.CacheKey;
 import com.rest.api.entity.User;
 import com.rest.api.entity.board.Board;
 import com.rest.api.entity.board.Post;
@@ -11,6 +12,8 @@ import com.rest.api.repo.UserJpaRepo;
 import com.rest.api.repo.board.BoardJpaRepo;
 import com.rest.api.repo.board.PostJpaRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -28,10 +31,12 @@ public class BoardService {
     private final UserJpaRepo userJpaRepo;
 
     // 게시판 이름으로 게시판을 조회. 없을경우 CResourceNotExistException 처리
+    @Cacheable(value = CacheKey.BOARD, key = "#boardName", unless = "result==null")
     public Board findBoard(String boardName) {
         return Optional.ofNullable(boardJpaRepo.findByName(boardName)).orElseThrow(CResourceNotExistException::new);
     }
     // 게시판 이름으로 게시물 리스트 조회.
+    @Cacheable(value = CacheKey.POSTS, key = "#boardName", unless = "#result == null")
     public List<Post> findPosts(String boardName) {
         return postJpaRepo.findByBoard(findBoard(boardName));
     }
@@ -40,12 +45,14 @@ public class BoardService {
         return postJpaRepo.findById(postId).orElseThrow(CResourceNotExistException::new);
     }
     // 게시물을 등록합니다. 게시물의 회원UID가 조회되지 않으면 CUserNotFoundException 처리합니다.
+//    @CacheEvict(value = CacheKey.POSTS, key = "#boardName")
     public Post writePost(String uid, String boardName, ParamsPost paramsPost) {
         Board board = findBoard(boardName);
         Post post = new Post(userJpaRepo.findByUid(uid).orElseThrow(CUserNotFoundException::new), board, paramsPost.getAuthor(), paramsPost.getTitle(), paramsPost.getContent());
         return postJpaRepo.save(post);
     }
     // 게시물을 수정합니다. 게시물 등록자와 로그인 회원정보가 틀리면 CNotOwnerException 처리합니다.
+    @CachePut(value = CacheKey.POST, key = "{#postId}")
     public Post updatePost(long postId, String uid, ParamsPost paramsPost) {
         Post post = getPost(postId);
         User user = post.getUser();
